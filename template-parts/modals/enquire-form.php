141705f619e42f68c2928772aa5469188a8d89fd
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-14 20:33:04 +0800

    Update enquire-form.php

--- template-parts/modals/enquire-form.php
+++ template-parts/modals/enquire-form.php
@@ -2,12 +2,12 @@
 <div class="modal fade" id="EnquireModal" tabindex="-1" aria-labelledby="EnquireModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="modal-header p-0">
                 <h1 class="modal-title fs-5" id="EnquireModalLabel">Enquire Now</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <?= do_shortcode('[contact-form-7 id="75457fa" title="Enquiry Form"]') ?>
+            <div class="modal-body p-0">
+                <?= do_shortcode('[contact-form-7 id="dfc6c96" title="Listing Enquiry Form : Popup"]') ?>
             </div>
         </div>
     </div>
