90090b67ce80d8a80fa481ac0ac287c465215956
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-14 20:14:01 +0800

    Update listing-grid-full-details.php

--- template-parts/shortcodes/listing-grid-full-details.php
+++ template-parts/shortcodes/listing-grid-full-details.php
@@ -37,9 +37,9 @@
       <div class="col-xl-5">
         <div class="listing-grid--right-inner">
           <div class="listing-grid-right-item">
-            <div class="image-box image-style mb-20 d-none d-lg-block" style="--fit: contain; --padding: 18%">
+            <!--<div class="image-box image-style mb-20 d-none d-lg-block" style="--fit: contain; --padding: 18%">
               <?= wp_get_attachment_image(189, 'large') ?>
-            </div>
+            </div>-->
             <h3 class="d-block d-lg-none">Swift Sprite Quattro FB 2024</h3>
             <div class="listing-inner--key-info d-none d-lg-block">
               <?php
