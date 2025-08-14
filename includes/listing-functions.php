a34553eca41e28e8812b3a1972f9b7f22856824a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-15 01:16:36 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -373,9 +373,10 @@ function listing__key_information($id, $category = 'caravans')
             if ($key_info['icon'] == false) {
                 $icon = get__theme_icons($key_info['id'] . '.svg');
             } else {
-                echo $key_info['icon'][$category];
+                $term = get_term_by('term_id', $category);
+                echo $key_info['icon'][$term->slug];
                 echo $category;
-                $icon = get__theme_icons($key_info['icon'][$category] . '.svg');
+                $icon = get__theme_icons($key_info['icon'][$term->slug] . '.svg');
             }
             $value = get__post_meta_by_id($id, $key_info['id']);
             echo '<li>';
