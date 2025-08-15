c8323e1d6c4862430c1ad664ac41cb2aecff2cf4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-15 17:32:17 +0800

    css

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -10,30 +10,6 @@ function latest_deals()
 add_shortcode('latest_deals', 'latest_deals');
 
 
-function listing_grid($atts)
-{
-    ob_start();
-    extract(
-        shortcode_atts(
-            array(
-                'style' => 'style-1',
-                'image_id' => 47
-            ),
-            $atts
-        )
-    );
-    $args['style'] = $style;
-    $args['image_id'] = $image_id;
-    get_template_part('template-parts/shortcodes/listing-grid', NULL, $args);
-    return ob_get_clean();
-}
-
-add_shortcode('listing_grid', 'listing_grid');
-
-
-
-
-
 function search_stock()
 {
     ob_start();
