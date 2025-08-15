3a034d268e9736ea21eeb9307974854f22860c09
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-15 17:04:25 +0800

    Update single-caravan.php

--- single-caravan.php
+++ single-caravan.php
@@ -135,7 +135,7 @@ $features = carbon_get_the_post_meta('features');
                                     <?= wp_get_attachment_image(189, 'large') ?>
                                 </div>-->
                             </div>
-                            <div class="listing-inner--description xs-margin-bottom">
+                            <div class="listing-inner--description xs-margin-bottom xs-margin-top">
                                 <h4 class="fs-35 heading mb-3 mb-lg-4">Description</h4>
                                 <div class="desc read--more">
                                     <?php the_content() ?>
