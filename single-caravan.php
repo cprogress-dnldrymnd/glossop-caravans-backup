3bf123a6e995d82868916b759483d0e30ee9228d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-15 17:27:44 +0800

    Update single-caravan.php

--- single-caravan.php
+++ single-caravan.php
@@ -326,13 +326,7 @@ if ($terms && !is_wp_error($terms)) {
         'numberposts' => 10,
         'post__not_in' => array(get_the_ID()),
         'orderby' => 'rand',
-        'tax_query' => array(
-            array(
-                'taxonomy' => 'listing_category',
-                'field' => 'term_id',
-                'terms' => $term_ids,
-            ),
-        ),
+       
     ));
     ?>
     <section class="listine-inner--related overflow-hidden pt-5 p5-lg-0">
