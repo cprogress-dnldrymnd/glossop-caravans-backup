5ed364988a5a07d7b64e6175e1963c8dbe8633de
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-15 15:39:18 +0800

    css

--- template-parts/modals/enquire-form.php
+++ template-parts/modals/enquire-form.php
@@ -2,11 +2,11 @@
 <div class="modal fade" id="EnquireModal" tabindex="-1" aria-labelledby="EnquireModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header px-0">
+            <div class="modal-header p-0 pb-4 mb-4">
                 <h1 class="modal-title fs-5" id="EnquireModalLabel">Enquire Now</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body px-0">
+            <div class="modal-body p-0">
                 <?= do_shortcode('[contact-form-7 id="dfc6c96" title="Listing Enquiry Form : Popup"]') ?>
             </div>
         </div>
