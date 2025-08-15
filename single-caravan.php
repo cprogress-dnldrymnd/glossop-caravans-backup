e4a1e083c477c0c68186df32b03c9a8c7d45443e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-15 17:37:36 +0800

    Update single-caravan.php

--- single-caravan.php
+++ single-caravan.php
@@ -343,7 +343,7 @@ if ($terms && !is_wp_error($terms)) {
                     <div class="swiper-wrapper">
                         <?php foreach ($related_posts as $post) { ?>
                             <div class="swiper-slide">
-                                <?= do_shortcode('[listing_grid image_id="195" style="style-2"]') ?>
+                                <?= listing_grid($id) ?>
                             </div>
                         <?php } ?>
                     </div>
