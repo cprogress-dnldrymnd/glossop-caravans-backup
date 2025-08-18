917329b1a1cb378f77fa2f225132ee4b345308e4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-19 00:07:40 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function () {
     read__more();
     listing_search_trigger();
 
-    jQuery('.listing-search--trigger').select2();
+    jQuery('.listing-search--trigger').select2({ allowClear: true });
 });
 
 function listing_search_trigger() {
