5a4028e39c4153966af8c993b14f4c9f3ee50a5a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-19 00:16:11 +0800

    Update listing-page.php

--- templates/listing-page.php
+++ templates/listing-page.php
@@ -48,7 +48,7 @@ $new_images_val[] = array(
 $new_images_val[] = array(
     'image_url' => 'https://manage.compare.group/images/caravans/photos/01755099069_Screenshot%202025-08-13%20163029.png'
 );
-update_post_meta(3579, 'images', $new_images_val);
+update_post_meta(3579, '_images', $new_images_val);
 ?>
 <div class="site-content listings ajax--section-js background-lightgray">
     <div class="container md-padding-top md-padding-bottom">
