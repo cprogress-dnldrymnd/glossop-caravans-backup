13d1a079993c7af761bceb96eaeedf9f9e9276b8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-19 19:00:36 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -194,6 +194,7 @@ function filter_options($args, $field_id)
 {
 	ob_start();
 	unset($args['posts_per_page']);
+	$field_id_val = '#' . $field_id;
 	$args['numberposts'] = -1;
 	$args['fields'] = 'ids';
 
@@ -210,7 +211,8 @@ function filter_options($args, $field_id)
 		$css['#year'][] = get__post_meta_by_id($post, 'year');
 		$css['#axle'][] = get__post_meta_by_id($post, 'axle');
 	}
-	unset($css['#' . $field_id]);
+	echo $field_id_val;
+	unset($css['#' . $field_id_val]);
 	echo '<style>';
 	foreach ($css as $key => $css_val) {
 		$css_val_format = css_val_format($css_val);
