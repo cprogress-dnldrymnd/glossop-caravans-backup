4d8fda2ac43c582e4623164b363a1b9d249600a7
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-19 20:51:49 +0800

    Update search-stock.php

--- template-parts/header/search-stock.php
+++ template-parts/header/search-stock.php
@@ -41,10 +41,10 @@ global $listing_fields;
                     <form action="" class="form-holder">
                         <div class="row align-items-end search-stock-row">
                             <div class="col-6 col-md-4 col-lg">
-                                <?= listing__filter_field('new_used', 'New-Used', 'Any', get__search_field_options('_new_used', 'caravans', false, false)) ?>
+                                <?= listing__filter_field('new_used', 'New-Used', 'Any', get__search_field_options('_new_used', 'caravans'), false, false) ?>
                             </div>
                             <div class="col-6 col-md-4 col-lg">
-                                <?= listing__filter_field('berths', 'Berths', 'Any', get__search_field_options('_berths', 'caravans'), false, false) ?>
+                                <?= listing__filter_field('berths', 'New-Used', 'Any', get__search_field_options('_new_used', 'caravans'), false, false) ?>
                             </div>
                             <div class="col-6 col-md-4 col-lg">
                                 <?= $listing_fields['make'] ?>
