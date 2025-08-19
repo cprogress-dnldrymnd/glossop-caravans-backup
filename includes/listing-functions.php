bbb0a0931f02e6a7d0671618d7d7a49d846eaa23
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-19 20:56:08 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -834,6 +834,9 @@ function listing__filter_field($id, $label, $placeholder = '', $available_option
                     'class'   => 'form-control-lg listing-search--trigger',
                     'options' => $options + $available_options
                 );
+                if($is_accordion==false) {
+                    $args['label'] = $label;
+                }
                 echo form_control($args);
             }
             if ($is_accordion) {
