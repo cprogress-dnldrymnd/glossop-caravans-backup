f1c70a70addb3fafc45c5a8c70b85d757afb24fd
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-19 17:24:26 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -9,6 +9,7 @@ jQuery(document).ready(function () {
     fixed_menu_link_mobile();
     read__more();
     listing_search_trigger();
+    model_options_trigger();
 
     //jQuery('.listing-search--trigger').select2();
 });
