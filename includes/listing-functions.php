782e0f0adf44ac45f343c8f163142a0f5ec5b23d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-19 17:37:01 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -803,7 +803,7 @@ function accordion__filter($id, $label, $placeholder = '', $available_options, $
         $placeholder_val = $placeholder;
     }
 ?>
-    <div class="accordion-item">
+    <div class="accordion-item accordion-item--<?= $id ?>">
         <h2 class="accordion-header">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapse-<?= $id ?>" aria-expanded="false"
