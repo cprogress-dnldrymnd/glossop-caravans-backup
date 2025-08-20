96697ff4040e51f0dad649ebdefecf0e90c289a6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-20 19:04:13 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -501,7 +501,7 @@ function listing__key_information($id, $category = 'caravans', $show = ['berths'
                 if ($key_info['icon'] == false) {
                     $icon = get__theme_icons($key_info['id'] . '.svg');
                 } else {
-                    $term = get_term_by('term_id', $category, 'listing_category');
+                    $term = get_term_by('slug', $category, 'listing_category');
                     $icon = get__theme_icons($key_info['icon'][$term->slug]);
                 }
                 echo '<li class="key--info-' . $key_info['id'] . '">';
