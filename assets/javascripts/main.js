ec97d04a1733e895c69bdb6fa6b236f9c52ca0ae
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-20 18:09:25 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -123,8 +123,7 @@ function listing_search_trigger() {
 
     jQuery('body').on('change', '.listing-filter .listing-search--trigger', function (e) {
         jQuery(this).find('option:not(.hidden)').each(function () {
-            let optionValue = jQuery(this).val(); // Get the value attribute
-            jQuery(this).addClass('hiddensdsds');
+            jQuery(this).addClass('dont-hide');
         });
         var filter_active = '';
         jQuery('html, body').animate({
